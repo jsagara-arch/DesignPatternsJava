d3417c323f3570f74a76dc69f898d9c14ec5d3f8
darshinigolli <darshinigolli@gmail.com>
2025-03-26 22:01:43 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
